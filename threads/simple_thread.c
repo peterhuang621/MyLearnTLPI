734fcd68190aa506dc0fda33b99d413204003a54
peterhuang621 <415443515@qq.com>
2024-09-08 22:28:15 +0800

    0908

---
+++ threads/simple_thread.c
@@ -0,0 +1,26 @@
+#include "../lib/error_functions.h"
+#include "../lib/tlpi_hdr.h"
+#include <pthread.h>
+
+static void *threadFunc(void *arg) {
+	char *s = (char *)arg;
+	printf("%s", s);
+	return (void *)strlen(s);
+}
+
+int main(int argc, char const *argv[]) {
+	pthread_t t1;
+	void *res;
+	int s;
+
+	s = pthread_create(&t1, NULL, threadFunc, "Hello world\n");
+	if (s != 0)
+		errExit(s, "pthread_create");
+	printf("Message from main()\n");
+	s = pthread_join(t1, &res);
+	if (s != 0)
+		errExitEN(s, "pthread_join");
+	printf("Thread returned %ld\n", (long)res);
+	exit(EXIT_SUCCESS);
+	return 0;
+}
