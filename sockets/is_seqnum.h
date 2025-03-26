c34ccfa2ecd3603db693f9b72f4cf06a8e2e2f5d
peterhuang621 <415443515@qq.com>
2025-03-26 22:31:19 +0800

    0326

---
+++ sockets/is_seqnum.h
@@ -0,0 +1,9 @@
+#include "../lib/get_num.h"
+#include "../lib/tlpi_hdr.h"
+#include "read_line.h"
+#include <netinet/in.h>
+#include <signal.h>
+#include <sys/socket.h>
+
+#define PORT_NUM "50000"
+#define INT_LEN 30
